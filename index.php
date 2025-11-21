fa53fc231c7b33aae28fbc533f4850fcaa828667
Ira Loginova <iralogionova2007@gmail.com>
2025-11-21 19:12:31 +0300

    правка

--- index.php
+++ index.php
@@ -10,6 +10,6 @@ $n2 = 0;
 if(is_numeric($number1) && is_numeric($number2)){
     $n1 = $number1;
     $n2 = $number2;
-}else echo "вводите только цифры!";
+}else echo "только цифры!";
 echo $n1 - $n2;
 ?>
