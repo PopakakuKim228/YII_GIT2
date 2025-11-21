100bb6d25c018d725aa00461f8bf02df694a1b9e
Ira Loginova <iralogionova2007@gmail.com>
2025-11-21 19:00:11 +0300

    addition

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$variants = array("ввести два числа", "выполнить сложение", "выполнить вычитание", "выполнить деление", "возвести число в степень");
+foreach($variants as $option){
+    echo $option . "\n";
+}
+
+$number1 = readline("первое число: ");
+$number2 = readline("ыторое число: ");
+$n1 = 0;
+$n2 = 0;
+
+if(is_numeric($number1) && is_numeric($number2)){
+    $n1 = $number1;
+    $n2 = $number2;
+}else echo "вводите только цифры!";
+
+echo $n1 - $n2;
+?>
