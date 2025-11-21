1bd638bba3987ce51febdfa8101ae631a229570b
Ira Loginova <iralogionova2007@gmail.com>
2025-11-21 19:11:20 +0300

    числа

--- index.php
+++ index.php
@@ -1,21 +1,19 @@
 <?php
-$variants = array("ввести два числа", "выполнить сложение", "выполнить вычитание", "выполнить деление", "возвести число в степень");
+$variants = array("1. ввести два числа", "2. выполнить сложение", "3. выполнить вычитание", "4. выполнить деление", "5. возвести число в степень");
 foreach($variants as $option){
     echo $option . "\n";
 }
 
-$number1 = readline("первое число: ");
-$number2 = readline("второе число: ");
+$commandInput = readLine('выберите число из списка');
+
+$result = 0;
 $n1 = 0;
 $n2 = 0;
 
+$number1 = readline("первое число: ");
+$number2 = readline("второе число:");
 if(is_numeric($number1) && is_numeric($number2)){
     $n1 = $number1;
     $n2 = $number2;
 }else echo "только цифры!";
-
-if($n2 != 0)
-    echo $n1 / $n2;
-else 
-    echo "ошибка: деление ноль!";
 ?>
