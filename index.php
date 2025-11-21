5acea560cf5247002aa8be1af9865a966fe74260
Ira Loginova <iralogionova2007@gmail.com>
2025-11-21 19:03:06 +0300

    сложение

--- index.php
+++ index.php
@@ -3,16 +3,13 @@ $variants = array("ввести два числа", "выполнить слож
 foreach($variants as $option){
     echo $option . "\n";
 }
-
 $number1 = readline("первое число: ");
 $number2 = readline("ыторое число: ");
 $n1 = 0;
 $n2 = 0;
-
 if(is_numeric($number1) && is_numeric($number2)){
     $n1 = $number1;
     $n2 = $number2;
 }else echo "вводите только цифры!";
-
 echo $n1 - $n2;
 ?>
